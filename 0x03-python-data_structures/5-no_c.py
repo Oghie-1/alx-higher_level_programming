916694cc5b623005357bd1ee055aa83e3add2e4f
afro <aigbemanuel@gmail.com>
2023-08-14 21:28:22 -0700

    added task 4-12

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def no_c(my_string):
+    new_string = ""
+    for item in my_string:
+        if item != "c" and item != "C":
+            new_string += item
+    return new_string
