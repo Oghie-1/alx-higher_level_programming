04806a916b0055333d24cb2ccb9381bcac53b353
afro <aigbemanuel@gmail.com>
2023-08-18 16:17:58 -0700

    initial commit

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,17 +2,17 @@
 
 
 size_t dlistint_len(const dlistint_t * h) {
-size_t count = 0
+size_t count = 0;
 /* Initialize a counter to keep track of the number of nodes */
 
 /* Traverse the linked list and count the elements */
 while (h != NULL) {
-count++
+count++;
 /* Increment the counter */
-h = h -> next
+h = h -> next;
 /* Move to the next node */
 }
 
-return count
+return count;
 /* Return the total number of elements */
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -14,6 +14,9 @@ struct dlistint_s *next;  /* Pointer to the next node */
 
 /* Function prototype for printing a doubly linked list */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
 
