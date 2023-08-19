48a4a9a49945d0fc5f074ea6a48c904a70ded21c
afro <aigbemanuel@gmail.com>
2023-08-19 04:51:47 -0700

    added task 6-8'

--- 0x17-doubly_linked_lists/5-main.c
+++ 0x17-doubly_linked_lists/5-main.c
@@ -1,16 +1,17 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
+* main - check the code
+*
+* Return: Always EXIT_SUCCESS.
+*/
 int main(void)
 {
 dlistint_t *head;
+dlistint_t *node;
 
 head = NULL;
 add_dnodeint_end(&head, 0);
@@ -22,7 +23,10 @@ add_dnodeint_end(&head, 98);
 add_dnodeint_end(&head, 402);
 add_dnodeint_end(&head, 1024);
 print_dlistint(head);
+node = get_dnodeint_at_index(head, 5);
+printf("%d\n", node->n);
 free_dlistint(head);
 head = NULL;
 return (EXIT_SUCCESS);
 }
+
