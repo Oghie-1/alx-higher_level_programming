82662c6d3862e1a787e770da46cf648541873f31
afro <aigbemanuel@gmail.com>
2023-08-29 03:16:42 -0700

    fixed str format issue

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if isinstance(my_list[i], int):
-                print(my_list[i], end="")
+                print("{:d}".format(my_list[i]))
                 count += 1
         print()
         return count
