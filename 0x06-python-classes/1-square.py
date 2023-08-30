b103db1a833910504ae7bb7570e498551e6313f2
afro <aigbemanuel@gmail.com>
2023-08-30 16:35:25 -0700

    refixed pep* err

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""" write a class Square that defines a square by: (based on 0-square.py """
+"""write a class Square. that defines a square by: (based on 0-square.py)"""
 
 class Square:
     """represent a square"""
 
     def __init__(self, size):
         """Initialize square"""
-        self.__size = size        
+        self.__size = size
