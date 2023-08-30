2392d3a1a5eb67b5ab99e37dfd351d116cc9c875
afro <aigbemanuel@gmail.com>
2023-08-30 16:55:48 -0700

    bugs

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
+
 """Define class Square. with exceptions if size not int"""
 
 
 class Square:
-    def __init__(self, size=0):
-        
-        """initialize new sqr"""
 
+
+    def __init__(self, size=0):
+        """initialize new sqr
+
+            size (int): size of new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-             raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size """returns size of sqr"""
 
