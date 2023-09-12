a4d37e5da5fed6784e8cffe414595d4977f5fcc3
afro <aigbemanuel@gmail.com>
2023-09-12 02:21:07 -0700

    added task 2 & 4

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 1-my_list
+Creates a class inheriting from the list class.
+"""
+
+class MyList(list):
+    """Custom list class that prints in ascending order."""
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        return sorted_list
