5c0914a06e3ea7834204488e090652ea326a90d5
afro <aigbemanuel@gmail.com>
2023-11-13 13:44:36 -0800

    added 103-object_fct.js

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+//functions that increments the integer value
+
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+	myObject.value++;
+}
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+
