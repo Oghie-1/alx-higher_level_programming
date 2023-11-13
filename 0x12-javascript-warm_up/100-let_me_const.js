7d14cdcbd25b2a505dad98e5d8931c71fe4694de
afro <aigbemanuel@gmail.com>
2023-11-13 08:33:43 -0800

    ädded adv files

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+//replaces value
+
+myVar = 333;
---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+//replace value
+
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
