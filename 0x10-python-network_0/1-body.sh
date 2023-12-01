d6cc5b24707de56e602a6b79beab14bb393ebc23
afro <aigbemanuel@gmail.com>
2023-12-01 04:39:31 -0800

    fixed .1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-
 # Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-# Use curl to fetch the URL and display the body for a 200 status code
-response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-if [ "$response" -eq 200 ]; then
-    body=$(curl -s "$url")
-    echo "Body of the response (Status Code 200):"
-    echo "$body"
-else
-    echo "Error: Status Code $response. Unable to display the body."
-fi
+curl -sfL "$1" -X Get
