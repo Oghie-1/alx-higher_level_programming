a26775043ea08044d67d0dee6767f4ac8ef4d2c0
afro <aigbemanuel@gmail.com>
2023-12-01 14:39:21 -0800

    added files

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Sends a POST request to a URL with specific variables in the body
-
-
-# Usage: ./5-post_params.sh <URL>
-
-url="$1"
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Send a POST request with specific variables in the body
-curl -s -X POST -d "email=$email&subject=$subject" "$url"
+#Send a POST request with specific variables in the body
+curl -sL "$1" -X POST -d "email=hr@holbertonschool.com&subjetc=I will always be here for PLD"
