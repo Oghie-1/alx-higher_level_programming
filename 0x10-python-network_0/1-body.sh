7fd8005f70e7223378d039a3ad42d20a034f65d6
afro <aigbemanuel@gmail.com>
2023-11-30 16:19:48 -0800

    added task1 refix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,21 @@
-#!/usr/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
+#!/bin/bash
 
 
-url=$1
-curl -sfL "url" -X GET
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+url="$1"
+
+# Use curl to fetch the URL and display the body for a 200 status code
+response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+if [ "$response" -eq 200 ]; then
+    body=$(curl -s "$url")
+    echo "Body of the response (Status Code 200):"
+    echo "$body"
+else
+    echo "Error: Status Code $response. Unable to display the body."
+fi
