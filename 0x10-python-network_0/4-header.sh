3a97a3f0cb28596e8f3a59733e7f478bd0c5ef41
afro <aigbemanuel@gmail.com>
2023-12-01 08:54:48 -0800

    refixed files

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with a specific header
-
-
-# Usage: ./4-header.sh <URL>
-
-url="$1"
-
-# Send a GET request with the X-School-User-Id header set to 98
-curl -s -H "X-School-User-Id: 98" "$url"
+#Send a GET request with the X-School-User-Id header set to 98
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
